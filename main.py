50b2a0d33889e0d8c0647d362684083379bdd9af
שניר אגו <snirego@Snirs-MacBook-Pro.local>
2022-08-17 15:48:47 +0300

    Snir Committed

--- main.py
+++ main.py
@@ -3,6 +3,45 @@
 #  -| main.py
 #  ---| sign_up.py
 #  ---| sign_in.py
-#  ---| creat_password.py
+#  ---| create_password.py
 #  ---| game.py
 # ------------------------------------------------------------
+# import files
+from genericpath import exists
+
+if exists("sign_up.py"):
+    from sign_up import *
+
+if exists("sign_in.py"):
+    from sign_in import *
+
+if exists("create_password.py"):
+    from create_password import *
+
+if exists("game.py"):
+    from game import *
+# ------------------------------------------------------------
+# main function
+def main():
+    print("Welcome to the game!")
+    print("Please choose an option:")
+    print("1. Sign up")
+    print("2. Sign in")
+    print("3. Create password")
+    print("4. Play game")
+    print("5. Exit")
+    option = int(input("Please enter your option: "))
+    if option == 1:
+        sign_up()
+    elif option == 2:
+        sign_in()
+    elif option == 3:
+        create_password()
+    elif option == 4:
+        game()
+    elif option == 5:
+        print("Thank you for playing!")
+        exit()
+    else:
+        print("Invalid option!")
+        main()
